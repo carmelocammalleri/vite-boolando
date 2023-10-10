bf358109b54bf822e879d033c1b3bdc0dbcb5961
Melozzo <carmecamma1995@gmail.com>
2023-10-10 17:06:47 +0200

    [fix]: structure app.Vue footer, header, main

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup>
+<script>
+  import Header from './components/Header.Vue';
+  import Main from './components/Main.Vue';
+  import Footer from './components/Footer.Vue';
 
+  export default {
+  name : 'App',
+  components : { 
+    Header,
+    Main,
+    Footer 
+}
+}
 </script>
 
 <template>
-  <h2> Ciao boolando</h2>
+
 </template>
